cc3df41831529f3bf4f38fb1f43b4e4ceefe9dd5
Joonho Kim <lippoint.surf0622@gmail.com>
2025-06-19 11:48:53 +0900

    vitest init

---
+++ src/components/input/TextInput.tsx
@@ -0,0 +1,18 @@
+import React, { useState, type ChangeEvent } from 'react'
+
+function TextInput() {
+    const [text, setText] = useState('')
+  return (
+    <div>
+        <input 
+            type="text" 
+            value={text} 
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)} 
+            placeholder="Enter your text"
+        />
+        <p>{text}</p>
+    </div>
+  )
+}
+
+export default TextInput
